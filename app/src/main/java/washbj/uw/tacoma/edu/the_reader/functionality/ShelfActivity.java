879c5424feb9ccbe14301c273f03df1a7b083c03
Iriohm <athelorii@gmail.com>
2017-03-09 10:07:29 -0800

    Added a bunch of comments.

--- app/src/main/java/washbj/uw/tacoma/edu/the_reader/functionality/ShelfActivity.java
+++ app/src/main/java/washbj/uw/tacoma/edu/the_reader/functionality/ShelfActivity.java
@@ -397,7 +397,6 @@ public class ShelfActivity extends AppCompatActivity
                 if ("primary".equalsIgnoreCase(type)) {
                     return Environment.getExternalStorageDirectory() + "/" + split[1];
                 }
-                // TODO handle non-primary volumes
             }
             // DownloadsProvider
             else if (isDownloadsDocument(uri)) {
